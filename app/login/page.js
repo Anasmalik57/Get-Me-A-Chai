21bdfe23fc514fb18cc00a0dc9d34b76209571a5
Anasmalik57 <itsanas474@gmail.com>
2025-03-05 09:43:09 +0530

    minor error fixes

--- app/login/page.js
+++ app/login/page.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 
 const Login = () => {
   const { data: session } = useSession();
@@ -21,13 +20,13 @@ const Login = () => {
       <div className="flex flex-col gap-4 min-h-screen items-center peer">
         {/* Google Login Button */}
         <button className="cursor-pointer flex items-center justify-center w-full md:w-60 bg-white border border-gray-300 rounded-xl shadow-md py-3 text-base font-medium text-gray-800 hover:bg-gray-300 transition-all duration-300 focus:outline-2 focus:outline-offset-2 active:scale-105">
-          <Image width={24} height={24} src={"/google.svg"} alt="Google Logo" className="h-6 w-6 mr-3" />
+          <img src={"/google.svg"} alt="Google Logo" className="h-6 w-6 mr-3" />
           <span>Continue with Google</span>
         </button>
 
         {/* LinkedIn Login Button */}
         <button className="cursor-pointer flex items-center justify-center w-full md:w-60 bg-white border border-gray-300 rounded-xl shadow-md py-3 text-base font-medium text-gray-800 hover:bg-gray-300 transition-all duration-300 focus:outline-2 focus:outline-offset-2 active:scale-105">
-          <Image width={24} height={24}
+          <img
             src={"/linkedin.svg"}
             alt="LinkedIn Logo"
             className="h-6 w-6 mr-3"
@@ -42,18 +41,18 @@ const Login = () => {
           }}
           className="cursor-pointer flex items-center justify-center w-full md:w-60 bg-white border border-gray-300 rounded-xl shadow-md py-3 text-base font-medium text-gray-800 hover:bg-gray-300 transition-all duration-300 focus:outline-2 focus:outline-offset-2 active:scale-105"
         >
-          <Image  width={24} height={24} src={"/github.svg"} alt="GitHub Logo" className="h-6 w-6 mr-3" />
+          <img src={"/github.svg"} alt="GitHub Logo" className="h-6 w-6 mr-3" />
           <span>Continue with GitHub</span>
         </button>
 
         {/* Twitter Login Button */}
         <button className="cursor-pointer flex items-center justify-center w-full md:w-60 bg-white border border-gray-300 rounded-xl shadow-md py-3 text-base font-medium text-gray-800 hover:bg-gray-300 transition-all duration-300 focus:outline-2 focus:outline-offset-2 active:scale-105">
-          <Image width={24} height={24} src={"/x.svg"} alt="Twitter Logo" className="h-6 w-6 mr-3" />
+          <img src={"/x.svg"} alt="Twitter Logo" className="h-6 w-6 mr-3" />
           <span>Continue with Twitter</span>
         </button>
         {/* Facebook Login Button */}
         <button className="cursor-pointer flex items-center justify-center w-full md:w-60 bg-white border border-gray-300 rounded-xl shadow-md py-3 text-base font-medium text-gray-800 hover:bg-gray-300 transition-all duration-300 focus:outline-2 focus:outline-offset-2 active:scale-105">
-          <Image width={24} height={24}
+          <img
             src={"/facebook.svg"}
             alt="Facebook Logo"
             className="h-6 w-6 mr-3"
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-        port: '',
-        pathname: '/account123/**',
-        search: '',
-      },
-    ],
-  },};
+const nextConfig = {};
 
 export default nextConfig;
